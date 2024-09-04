d089fc796550e62d0e89d7843e18753901f9d2e4
liujian <904275123@qq.com>
2024-09-04 17:24:15 +0800

    Initial commit​

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 所有单位的基类
+/// </summary>
+public class BaseObject : MonoBehaviour
+{
+
+    private float hp;
+    public AudioSource audioSource;
+    public List<AudioClip> audioClips;
+    public GameObject lootObject; //掉落的物品
+
+    // Hp修改时自动判断死亡
+    public float Hp { get => hp;
+        set {
+            hp = value;
+            if(hp<=0){
+                hp = 0;
+                Dead();
+            }
+            OnHpUpdate(); //自动调用Hp更新逻辑
+        }
+    }
+
+    protected void PlayAudio(int index){
+        audioSource.PlayOneShot(audioClips[index]);
+    }
+
+    /// <summary>
+    /// 当生命值更新自动调用
+    /// </summary>
+    protected virtual void OnHpUpdate(){}
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    protected virtual void Dead(){
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
